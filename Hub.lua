1390dbd0b40b0588e9b0062579f6dfe6b934adc5
CeleryHub <bladeballchampion@gmail.com>
2024-08-13 18:41:13 -0400

    Update Hub.lua

--- Hub.lua
+++ Hub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/CeleryHub/CeleryHub/main/pHub/I%20Heart%20Celery.luau")()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CeleryHub/CeleryHub/main/pHub/I%20Heart%20Celery.luau"))()
